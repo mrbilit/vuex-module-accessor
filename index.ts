c91a105e47ba92c26974698141418be5ca7c560c
MehranTaheri <mehrant7510@gmail.com>
2020-07-22 14:21:45 +0430

    test

--- index.ts
+++ index.ts
@@ -1,2 +1,7 @@
 import ModuleAccessor from './src/ModuleAccessor';
+import Module from './src/Module';
+import { mutation, utility } from './src/Decorators';
+import { typedMapGetters, typedMapState } from './src/Mappers';
+
+export { Module, mutation, utility, typedMapGetters, typedMapState };
 export default ModuleAccessor;
