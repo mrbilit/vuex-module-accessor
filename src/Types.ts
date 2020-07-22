fee8d0ac3d54a321b1bfa9048c8ee4259e2021b7
MehranTaheri <mehrant7510@gmail.com>
2020-07-22 14:29:20 +0430

    add types

---
+++ src/Types.ts
@@ -0,0 +1,5 @@
+import Module from './Module';
+import ModuleAccessor from './ModuleAccessor';
+
+export type ExtractState<T> = T extends Module<infer X> ? X : never;
+export type ExtractModule<T> = T extends ModuleAccessor<infer X, infer Y> ? X : never;
