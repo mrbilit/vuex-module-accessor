cce20e8a113b33ce814f11db97086a6d5269fc39
MehranTaheri <mehrant7510@gmail.com>
2020-12-18 20:55:46 +0330

    Add some config for publish

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import ModuleAccessor from './ModuleAccessor';
+import Module from './Module';
+import { mutation, utility } from './Decorators';
+import { typedMapGetters, typedMapState } from './Mappers';
+
+export {
+	ModuleAccessor,
+	Module,
+	mutation,
+	utility,
+	typedMapGetters,
+	typedMapState
+};
