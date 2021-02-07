abf74ad80504ebba3a1342961f8fd1d7dfa3e83e
MehranTaheri <mehrant7510@gmail.com>
2021-02-07 14:18:01 +0330

    Export types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import ModuleAccessor from './ModuleAccessor';
 import Module from './Module';
 import { mutation, utility } from './Decorators';
 import { typedMapGetters, typedMapState } from './Mappers';
+export { ModuleGetter, ExtractModule, ExtractState } from './Types';
 
 export {
 	ModuleAccessor,
